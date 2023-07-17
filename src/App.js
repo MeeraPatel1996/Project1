dee16f22cdf7c0aca848d8a7c32ded416e226776
MeeraPatel1996 <meerarpatel1996@gmail.com>
2023-07-17 10:40:12 +0530

    react init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Sumit</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus ut eos quasi assumenda temporibus velit dolorum consequatur inventore aliquid, unde quidem recusandae dolor tenetur<br /> commodi minima labore cupiditate facere animi!</p>
+    </div>
+  );
+}
+
+export default App;
